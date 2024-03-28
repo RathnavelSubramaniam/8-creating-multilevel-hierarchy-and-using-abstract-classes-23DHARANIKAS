430fb6a34b7c0096bc3c79b183a48c4adf464f3d
1U23CA030DHARANIKA <139735655+23DHARANIKAS@users.noreply.github.com>
2024-03-28 04:40:38 +0000

    save

--- Practical8.java
+++ Practical8.java
@@ -1,26 +1,77 @@
-// Animal.java
-public abstract class Animal {
-// Basic attributes and methods including abstract sound method
+abstract class Animal {
+protected String species;
+protected int age;
+
+public Animal (String species, int age){
+    this.species = species;
+    this.age = age;
+}
+public abstract void sound();
+
+public String toString(){
+   return "Species: " + species +", " +"Age: " +age;
+}
+public boolean equals(Object obj){
+   if(this == obj) return true;
+   if(obj == null || getClass()!= obj.getClass()) return false;
+
+   Animal otherAnimal = (Animal)obj;
+   return species.equals(otherAnimal.species) && age==otherAnimal.age;
+}
 }
 
-// Mammal.java
-public class Mammal extends Animal {
-// Additional attributes and methods
+ class Mammal extends Animal {
+    private String habit;
+ 
+    public Mammal(String species, int age, String habit){
+       super(species,age);
+       this.habit = habit;
+ }
+ public void sound(){
+
+ }
 }
 
-// Bird.java
-public class Bird extends Animal {
-// Additional attributes and methods
+ class Bird extends Animal {
+     private String featherColor;
+
+     public Bird(String species, int age, String featherColor){
+        super(species,age);
+        this.featherColor = featherColor;
+     }
+public void sound(){
+
+}
 }
 
-// Parrot.java
-public class Parrot extends Bird {
-// Additional characteristics specific to Parrot
+ class Parrot extends Bird {
+private boolean canSpeak;
+
+ public Parrot(String species, int age, String habit, String featherColor, boolean canSpeak){
+    super(species,age,featherColor);
+    this.canSpeak = canSpeak;
+ }
+ public void sound(){
+   if(canSpeak){
+    System.out.println("Parrot speaking");
+   }
+   else{
+    System.out.println("Parrot sound");
+   }
+ }
 }
 
-// Main.java
-public class Main {
+public class Practical8 {
 public static void main(String[] args) {
-// Instantiate and demonstrate object class methods
+  Mammal lion = new Mammal("Lion", 5, "Grasslands");
+  Bird eagle = new Bird("Eagle", 3, "Brown");
+  Parrot talkingParrot = new Parrot ("Talking Parrot", 2, "Speech", "Green", true);
+
+  System.out.println(lion);
+  System.out.println(eagle);
+  System.out.println(talkingParrot);
+
+  System.out.println("Are lion and eagle equal? " +lion.equals(eagle));
+  System.out.println("Are lion and lion equal? " +lion.equals(new Mammal("Lion", 5, "Grasslands")));
 }
 }
